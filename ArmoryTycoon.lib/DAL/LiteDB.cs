8d1f6811d8c3f74958f93d89f14783277738f1d2
Jarred Capellman <jcapellman@hotmail.com>
2021-01-03 08:56:34 -0600

    Change: Refactored the LiteDB to use the Game class

--- ArmoryTycoon.lib/DAL/LiteDB.cs
+++ ArmoryTycoon.lib/DAL/LiteDB.cs
@@ -9,11 +9,11 @@ namespace ArmoryTycoon.lib.DAL
     {
         private const string DB_FILENAME = "game.db";
         
-        public override bool SaveNewGame(SaveGame game)
+        public override bool SaveNewGame(Game game)
         {
             using var db = new LiteDatabase(DB_FILENAME);
 
-            return db.GetCollection<SaveGame>().Insert(game) > 0;
+            return db.GetCollection<Game>().Insert(game) > 0;
         }
     }
 }
