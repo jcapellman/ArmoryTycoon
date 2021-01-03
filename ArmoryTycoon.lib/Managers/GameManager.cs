c289b565374dd2b80de42b61be75a833ac398b20
Jarred Capellman <jcapellman@hotmail.com>
2021-01-03 08:57:58 -0600

    Added: GameManager class

---
+++ ArmoryTycoon.lib/Managers/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using ArmoryTycoon.lib.Common;
+using ArmoryTycoon.lib.DAL.Objects;
+
+namespace ArmoryTycoon.lib.Managers
+{
+    public class GameManager
+    {
+        public void LoadGame(int gameId)
+        {
+            
+        }
+
+        public void DeleteGame(int gameId)
+        {
+
+        }
+    }
+}
