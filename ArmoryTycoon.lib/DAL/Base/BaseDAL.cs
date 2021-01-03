ca241680856d6fe85ac01af26977408d9172b3d8
Jarred Capellman <jcapellman@hotmail.com>
2021-01-03 09:06:33 -0600

    Added: Game Operation functions in the BaseDAL are defined

--- ArmoryTycoon.lib/DAL/Base/BaseDAL.cs
+++ ArmoryTycoon.lib/DAL/Base/BaseDAL.cs
@@ -1,9 +1,17 @@
-﻿using ArmoryTycoon.lib.DAL.Objects;
+﻿using System.Collections.Generic;
+
+using ArmoryTycoon.lib.DAL.Objects;
 
 namespace ArmoryTycoon.lib.DAL.Base
 {
     public abstract class BaseDAL
     {
         public abstract bool SaveNewGame(Game game);
+
+        public abstract Dictionary<int, string> GetSaveGameList();
+
+        public abstract bool DeleteSaveGame(int gameId);
+
+        public abstract Game LoadSaveGame(int gameId);
     }
 }
