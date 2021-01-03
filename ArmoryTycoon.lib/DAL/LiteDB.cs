b05544a51b1e7fc02a56d6418056a8a1b56ffde0
Jarred Capellman <jcapellman@hotmail.com>
2021-01-03 09:07:25 -0600

    Added: Implementation of the new BaseDAL abstract methods

--- ArmoryTycoon.lib/DAL/LiteDB.cs
+++ ArmoryTycoon.lib/DAL/LiteDB.cs
@@ -1,4 +1,7 @@
-﻿using ArmoryTycoon.lib.DAL.Base;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using ArmoryTycoon.lib.DAL.Base;
 using ArmoryTycoon.lib.DAL.Objects;
 
 using LiteDB;
@@ -14,6 +17,27 @@ namespace ArmoryTycoon.lib.DAL
             using var db = new LiteDatabase(DB_FILENAME);
 
             return db.GetCollection<Game>().Insert(game) > 0;
+        }
+
+        public override Dictionary<int, string> GetSaveGameList()
+        {
+            using var db = new LiteDatabase(DB_FILENAME);
+
+            return db.GetCollection<Game>().FindAll().ToDictionary(a => a.Id, b => b.Name);
+        }
+
+        public override bool DeleteSaveGame(int gameId)
+        {
+            using var db = new LiteDatabase(DB_FILENAME);
+
+            return db.GetCollection<Game>().Delete(gameId);
+        }
+
+        public override Game LoadSaveGame(int gameId)
+        {
+            using var db = new LiteDatabase(DB_FILENAME);
+
+            return db.GetCollection<Game>().FindById(gameId);
         }
     }
 }
