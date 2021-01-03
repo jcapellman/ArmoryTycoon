61cae905269f7598a5712eff04967c840adb22be
Jarred Capellman <jcapellman@hotmail.com>
2021-01-03 08:56:10 -0600

    Change: Refactored the BaseDAL

--- ArmoryTycoon.lib/DAL/Base/BaseDAL.cs
+++ ArmoryTycoon.lib/DAL/Base/BaseDAL.cs
@@ -4,6 +4,6 @@ namespace ArmoryTycoon.lib.DAL.Base
 {
     public abstract class BaseDAL
     {
-        public abstract bool SaveNewGame(SaveGame game);
+        public abstract bool SaveNewGame(Game game);
     }
 }
