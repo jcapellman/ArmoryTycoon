c78024ce13d3c3f9c6a927c29e9b30ab9cfa0e92
Jarred Capellman <jcapellman@hotmail.com>
2021-01-04 18:18:49 -0600

    Added: Initial work on the LoadGameViewModel class

---
+++ ArmoryTycoon.UWP/ViewModels/LoadGameViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using ArmoryTycoon.lib.DAL.Base;
+
+namespace ArmoryTycoon.UWP.ViewModels
+{
+    public class LoadGameViewModel : INotifyPropertyChanged
+    {
+        public class LoadGameListing
+        {
+            public int ID { get; set; }
+
+            public string Name { get; set; }
+        }
+
+        private List<LoadGameListing> _gameListing;
+
+        public List<LoadGameListing> GameListing
+        {
+            get => _gameListing;
+
+            set
+            {
+                _gameListing = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        private BaseDAL _baseDal;
+
+        public void Initialize(BaseDAL baseDal)
+        {
+            _baseDal = baseDal;
+
+            GameListing = _baseDal.GetSaveGameList().Select(a => new LoadGameListing
+            {
+                ID = a.Key, Name = a.Value
+            }).ToList();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
