f6167a160dea00d86102c957ad634b2137546bc9
Jarred Capellman <jcapellman@hotmail.com>
2021-01-04 18:18:27 -0600

    Added: Initial work on the LoadGamePage class

---
+++ ArmoryTycoon.UWP/Pages/LoadGamePage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using ArmoryTycoon.lib.DAL;
+using ArmoryTycoon.UWP.ViewModels;
+
+namespace ArmoryTycoon.UWP.Pages
+{
+    public sealed partial class LoadGamePage : Page
+    {
+        private LoadGameViewModel Vm => (LoadGameViewModel) DataContext;
+        
+        public LoadGamePage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new LoadGameViewModel();
+            
+            Vm.Initialize(new LitedbDAL());
+        }
+
+        private void btnBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+    }
+}
