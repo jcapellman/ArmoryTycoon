526c826de2dc1c9013721ab00729bcbe57aa15b0
Jarred Capellman <jcapellman@hotmail.com>
2021-01-06 19:12:22 -0600

    Added: Handling Parameters on Navigate

--- ArmoryTycoon.UWP/Pages/MainGamePage.xaml.cs
+++ ArmoryTycoon.UWP/Pages/MainGamePage.xaml.cs
@@ -1,30 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+using ArmoryTycoon.lib.DAL.Objects;
+using ArmoryTycoon.UWP.ViewModels;
 
 namespace ArmoryTycoon.UWP.Pages
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainGamePage : Page
     {
+        private MainGameViewModel Vm => (MainGameViewModel) DataContext;
+        
         public MainGamePage()
         {
             this.InitializeComponent();
+
+            DataContext = new MainGameViewModel();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            Vm.CurrentGame = (Game) e.Parameter;
         }
     }
 }
