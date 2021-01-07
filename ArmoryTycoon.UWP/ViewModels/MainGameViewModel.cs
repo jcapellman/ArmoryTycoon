3387c618f9d76425ce5f5919b74e8ef3c45d77de
Jarred Capellman <jcapellman@hotmail.com>
2021-01-06 19:12:51 -0600

    Added: Initial work on the MainGameViewModel

---
+++ ArmoryTycoon.UWP/ViewModels/MainGameViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using ArmoryTycoon.lib.DAL.Objects;
+
+namespace ArmoryTycoon.UWP.ViewModels
+{
+    public class MainGameViewModel : INotifyPropertyChanged
+    {
+        private Game _currentGame;
+
+        public Game CurrentGame
+        {
+            get => _currentGame;
+
+            set
+            {
+                _currentGame = value;
+
+                OnPropertyChanged();
+            }
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
