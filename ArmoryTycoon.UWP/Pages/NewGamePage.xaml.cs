356046b5c23ef440a54aa3ae01c73e77dc498e36
Jarred Capellman <jcapellman@hotmail.com>
2021-01-07 18:46:31 -0600

    Added: Initial work on the NewGamePage

---
+++ ArmoryTycoon.UWP/Pages/NewGamePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+using ArmoryTycoon.UWP.ViewModels;
+
+namespace ArmoryTycoon.UWP.Pages
+{
+    public sealed partial class NewGamePage : Page
+    {
+        private NewGameViewModel Vm => (NewGameViewModel) DataContext;
+        
+        public NewGamePage()
+        {
+            InitializeComponent();
+
+            DataContext = new NewGameViewModel();
+        }
+
+        private void btnBack_OnClick(object sender, RoutedEventArgs e)
+        {
+            Frame.GoBack();
+        }
+
+        private void btnNewGame_OnClick(object sender, RoutedEventArgs e)
+        {
+            var game = Vm.CreateGame();
+
+            Frame.Navigate(typeof(MainGamePage), game);
+        }
+    }
+}
