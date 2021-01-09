0bf2aa05f3bc2480dc7b787f87ca927a6c9e755f
Jarred Capellman <jcapellman@hotmail.com>
2021-01-08 20:05:14 -0600

    Added: Initial work on the OptionsPage

---
+++ ArmoryTycoon.UWP/Pages/OptionsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+
+using ArmoryTycoon.UWP.ViewModels;
+
+namespace ArmoryTycoon.UWP.Pages
+{
+    public sealed partial class OptionsPage : Page
+    {
+        public OptionsPage()
+        {
+            this.InitializeComponent();
+
+            DataContext = new OptionsViewModel();
+        }
+    }
+}
---
+++ ArmoryTycoon.UWP/ViewModels/OptionsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using ArmoryTycoon.UWP.Annotations;
+
+namespace ArmoryTycoon.UWP.ViewModels
+{
+    public class OptionsViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
