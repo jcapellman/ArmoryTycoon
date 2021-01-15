d4960a6682ff6836d48c1dcdd5a137d06ba5c622
Jarred Capellman <jcapellman@hotmail.com>
2021-01-15 17:13:42 -0600

    Added: ArmoryItemType Enum

---
+++ ArmoryTycoon.lib/Enums/ArmoryItemType.cs
@@ -0,0 +1,11 @@
+﻿namespace ArmoryTycoon.lib.Enums
+{
+    public enum ArmoryItemType
+    {
+        REVOLVER,
+        SEMI_AUTO_HANDGUN,
+        SEMI_AUTO_SHOTGUN,
+        BOLT_ACTION_RIFLE,
+        SEMI_AUTO_RIFLE
+    }
+}
