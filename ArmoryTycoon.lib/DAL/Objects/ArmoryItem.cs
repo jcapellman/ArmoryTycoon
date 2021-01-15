dbb9e138e5851d11c34b151801daf02d005fd407
Jarred Capellman <jcapellman@hotmail.com>
2021-01-15 16:56:08 -0600

    Added: Initial work on the ArmoryItem Class

---
+++ ArmoryTycoon.lib/DAL/Objects/ArmoryItem.cs
@@ -0,0 +1,23 @@
+﻿namespace ArmoryTycoon.lib.DAL.Objects
+{
+    public class ArmoryItem
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public float Price { get; set; }
+        
+        public int YearIntroduced { get; set; }
+        
+        public int NumberSold { get; set; }
+        
+        public int Rating_BuildQuality { get; set; }
+        
+        public int Rating_Reliability { get; set; }
+        
+        public int Rating_Capacity { get; set; }
+        
+        public int Rating_Price { get; set; }
+    }
+}
