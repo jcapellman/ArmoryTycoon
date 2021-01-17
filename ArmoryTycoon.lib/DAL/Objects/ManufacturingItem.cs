88ee537c53daa4c1be7f62c1bc511f6693722955
Jarred Capellman <jcapellman@hotmail.com>
2021-01-16 19:44:41 -0600

    Added: ManufacturingItem class

---
+++ ArmoryTycoon.lib/DAL/Objects/ManufacturingItem.cs
@@ -0,0 +1,28 @@
+﻿namespace ArmoryTycoon.lib.DAL.Objects
+{
+    public class ManufacturingItem
+    {
+        public int QuarterlyCost { get; set; }
+
+        public int MaxThroughput { get; set; }
+
+        public int MaxQuality { get; set; }
+        
+        public int Worth { get; set; }
+
+        public int Level { get; set; }
+        
+        public ManufacturingItem()
+        {
+            MaxThroughput = 1000;
+
+            MaxQuality = 10;
+
+            Worth = 0;
+
+            QuarterlyCost = 25000;
+
+            Level = 1;
+        }
+    }
+}
