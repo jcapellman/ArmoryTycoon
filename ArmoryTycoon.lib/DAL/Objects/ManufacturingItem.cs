df1de81a2f1cf696683a60709e8c8d09fb2ea926
Jarred Capellman <jcapellman@hotmail.com>
2021-01-17 08:46:11 -0600

    Added: Level up support in the ManufacturingItem class

--- ArmoryTycoon.lib/DAL/Objects/ManufacturingItem.cs
+++ ArmoryTycoon.lib/DAL/Objects/ManufacturingItem.cs
@@ -2,6 +2,12 @@
 {
     public class ManufacturingItem
     {
+        private const int Multiplier_Quality = 2;
+        private const int Multiplier_Throughput = 1000;
+        private const int Multiplier_Cost = 25000;
+        private const int Initial_Level = 1;
+        private const int Multiplier_Worth = 50000;
+        
         public int QuarterlyCost { get; set; }
 
         public int MaxThroughput { get; set; }
@@ -14,15 +20,27 @@
         
         public ManufacturingItem()
         {
-            MaxThroughput = 1000;
+            MaxThroughput = Multiplier_Throughput;
 
-            MaxQuality = 10;
+            MaxQuality = Multiplier_Quality;
 
             Worth = 0;
 
-            QuarterlyCost = 25000;
+            QuarterlyCost = Multiplier_Cost;
 
-            Level = 1;
+            Level = Initial_Level;
+        }
+
+        public int NextLevelCost => Level * Multiplier_Cost;
+
+        public void LevelUp()
+        {
+            Level++;
+            
+            QuarterlyCost = Level * Multiplier_Cost;
+            MaxQuality += Multiplier_Quality;
+            MaxThroughput = Level * Multiplier_Throughput;
+            Worth = Level * Multiplier_Worth;
         }
     }
 }
