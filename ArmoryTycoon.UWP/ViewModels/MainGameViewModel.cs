2bff2558a52d0d2f567eb083e39b21b2e17e1867
Jarred Capellman <jcapellman@hotmail.com>
2021-01-18 18:10:34 -0600

    Added: SelectedArmoryItem and NewArmoryItem for form use

--- ArmoryTycoon.UWP/ViewModels/MainGameViewModel.cs
+++ ArmoryTycoon.UWP/ViewModels/MainGameViewModel.cs
@@ -21,6 +21,34 @@ namespace ArmoryTycoon.UWP.ViewModels
             }
         }
 
+        private ArmoryItem _newArmoryItem;
+
+        public ArmoryItem NewArmoryItem
+        {
+            get => _newArmoryItem;
+
+            set
+            {
+                _newArmoryItem = value;
+                
+                OnPropertyChanged();
+            }
+        }
+
+        private ArmoryItem _selectedArmoryItem;
+
+        public ArmoryItem SelectedArmoryItem
+        {
+            get => _selectedArmoryItem;
+
+            set
+            {
+                _selectedArmoryItem = value;
+                
+                OnPropertyChanged();
+            }
+        }
+        
         private bool _upgradeAvailable;
 
         public bool UpgradeAvailable
@@ -64,8 +92,19 @@ namespace ArmoryTycoon.UWP.ViewModels
             UpgradeText = $"Next upgrade cost is ${CurrentGame.Manufacturing.NextLevelCost}";
             
             OnPropertyChanged(nameof(CurrentGame));
+
+            NewArmoryItem = new ArmoryItem();
+            
+            OnPropertyChanged(nameof(NewArmoryItem));
         }
 
+        public void AddArmoryItem()
+        {
+            CurrentGame.AddArmoryItem(NewArmoryItem);
+            
+            UpdateBindings();
+        }
+        
         public void UpgradeManufacturing()
         {
             CurrentGame.Cash -= CurrentGame.Manufacturing.NextLevelCost;
